a7f4c6e42d9bda4e687f7e88910feffcaad5207f
fuzhengwei <184172133@qq.com>
2020-04-15 23:25:35 +0800

    公众号：bugstack虫洞栈 | NettyServer群发消息

---
+++ src/itstack-demo-netty/itstack-demo-netty-1-06/src/main/java/org/itstack/demo/netty/server/MyServerHandler.java
@@ -0,0 +1,64 @@
+package org.itstack.demo.netty.server;
+
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.socket.SocketChannel;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * 虫洞栈：https://bugstack.cn
+ * 公众号：bugstack虫洞栈  ｛获取学习源码｝
+ * Create by fuzhengwei on 2019
+ */
+public class MyServerHandler extends ChannelInboundHandlerAdapter {
+
+    /**
+     * 当客户端主动链接服务端的链接后，这个通道就是活跃的了。也就是客户端与服务端建立了通信通道并且可以传输数据
+     */
+    @Override
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+        //当有客户端链接后，添加到channelGroup通信组
+        ChannelHandler.channelGroup.add(ctx.channel());
+        //日志信息
+        SocketChannel channel = (SocketChannel) ctx.channel();
+        System.out.println("链接报告开始");
+        System.out.println("链接报告信息：有一客户端链接到本服务端");
+        System.out.println("链接报告IP:" + channel.localAddress().getHostString());
+        System.out.println("链接报告Port:" + channel.localAddress().getPort());
+        System.out.println("链接报告完毕");
+        //通知客户端链接建立成功
+        String str = "通知客户端链接建立成功" + " " + new Date() + " " + channel.localAddress().getHostString() + "\r\n";
+        ctx.writeAndFlush(str);
+    }
+
+    /**
+     * 当客户端主动断开服务端的链接后，这个通道就是不活跃的。也就是说客户端与服务端的关闭了通信通道并且不可以传输数据
+     */
+    @Override
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+        System.out.println("客户端断开链接" + ctx.channel().localAddress().toString());
+        // 当有客户端退出后，从channelGroup中移除。
+        ChannelHandler.channelGroup.remove(ctx.channel());
+    }
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+        //接收msg消息{与上一章节相比，此处已经不需要自己进行解码}
+        System.out.println(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date()) + " 接收到消息：" + msg);
+        //收到消息后，群发给客户端
+        String str = "服务端收到：" + new Date() + " " + msg + "\r\n";
+        ChannelHandler.channelGroup.writeAndFlush(str);
+    }
+
+    /**
+     * 抓住异常，当发生异常的时候，可以做一些相应的处理，比如打印日志、关闭链接
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+        ctx.close();
+        System.out.println("异常信息：\r\n" + cause.getMessage());
+    }
+
+}
