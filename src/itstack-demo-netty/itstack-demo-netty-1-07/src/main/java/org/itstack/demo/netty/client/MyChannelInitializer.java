e21e6cbfe23d4ee625ddf28c5d3f71df4657d71b
fuzhengwei <184172133@qq.com>
2020-04-15 23:26:00 +0800

    公众号：bugstack虫洞栈 | 嗨！NettyClient

---
+++ src/itstack-demo-netty/itstack-demo-netty-1-07/src/main/java/org/itstack/demo/netty/client/MyChannelInitializer.java
@@ -0,0 +1,20 @@
+package org.itstack.demo.netty.client;
+
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.socket.SocketChannel;
+
+/**
+ * 虫洞栈：https://bugstack.cn
+ * 公众号：bugstack虫洞栈  ｛获取学习源码｝
+ * Create by fuzhengwei on 2019
+ */
+public class MyChannelInitializer extends ChannelInitializer<SocketChannel> {
+
+    @Override
+    protected void initChannel(SocketChannel channel) throws Exception {
+        System.out.println("链接报告开始");
+        System.out.println("链接报告信息：本客户端链接到服务端。channelId：" + channel.id());
+        System.out.println("链接报告完毕");
+    }
+
+}
