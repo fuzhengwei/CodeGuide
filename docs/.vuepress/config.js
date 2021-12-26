f9189c0bb9f69839900efdbd870b1e738a3ef651
fuzhengwei <184172133@qq.com>
2021-12-26 15:48:57 +0800

    小傅哥,docs：加入项目：[https://t.zsxq.com/jAi2nUf](https://t.zsxq.com/jAi2nUf) - 加入后在星球置顶消息可以申请加入项目组，`公众号：bugstack虫洞栈 回复：星球 可以获得加入优惠券`

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1281,7 +1281,6 @@ function getBarProjectLottery() {
             children: [
                 "Part-3/第01节：UI工程搭建.md",
                 "Part-3/第02节：活动列表数据展示.md",
-                "Part-3/第03节：用户抽奖数据展示.md",
             ]
         },
         {
