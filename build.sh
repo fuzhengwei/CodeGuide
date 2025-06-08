9ae6a9676337aa5439d47c1956025b8f3a5b4ad4
fuzhengwei <184172133@qq.com>
2025-06-08 11:01:04 +0800

    feat：build.sh 配置

--- build.sh
+++ build.sh
@@ -1,3 +1,10 @@
 NODE_OPTIONS=--max-old-space-size=24096 npm run build
 
 # 优惠券地址；https://bugstack.cn/images/article/zsxq/zsxq-youhuiquan.png
+# node 版本切换；https://www.cnblogs.com/fengchong0925/p/18489658
+# 全局安装；npm install -g n
+# 安装指定node版本；sudo -E n 18.16.0
+# 查看已安装的版本列表；n list
+# 删除指定版本；n rm 16.13.2
+# 切换node版本；sudo n
+# 查看切换版本；node -v npm -v
